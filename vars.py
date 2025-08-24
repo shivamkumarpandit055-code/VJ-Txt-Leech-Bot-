40d13edaf14673acaf7af980f27038794d292108
shivamkumarpandit055-code <shivamkumarpandit055@gmail.com>
2025-08-25 02:20:56 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,4 +1,4 @@
-# Don't Remove Credit Tg - @Mr..@radharadhe0
+# Don't Remove Credit Tg - @VJ_botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
@@ -7,6 +7,7 @@ from os import environ
 API_ID = int(environ.get("API_ID", "22448724"))
 API_HASH = environ.get("API_HASH", "4dcc0e5b700ad50b1f878e6f1e44c172")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
+
 
 
 
